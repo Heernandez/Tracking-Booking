56d1228b477498eaf31a433380efd6ce1ed3da21
Luis Hernandez <luishernandez@utp.edu.co>
2024-08-29 01:35:30 -0500

    ajuste conf cors

--- src/index.js
+++ src/index.js
@@ -12,7 +12,13 @@ dotenv.config();
 
 const app = express();
 // Se habilita CORS para todas las rutas
-app.use(cors());
+//app.use(cors());
+
+app.use(cors({
+    origin: 'https://heernandezdev.com',
+    methods: ['GET', 'POST', 'OPTIONS'],
+    allowedHeaders: ['*', 'Content-Type', 'Accept', 'Authorization'],
+}));
 app.use(helmet());
 
 const port = process.env.PORT || 3000;
